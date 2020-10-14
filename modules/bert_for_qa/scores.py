e9037387d4753d628f6f3e1b9029cc30a241f18d
AndreaSottana <48888970+AndreaSottana@users.noreply.github.com>
2020-10-14 15:21:59 +0100

    add f1 score

--- modules/bert_for_qa/scores.py
+++ modules/bert_for_qa/scores.py
@@ -1,9 +1,6 @@
 import torch
+import numpy as np
 from typing import List, Union
-import logging
-
-
-logger = logging.getLogger(__name__)
 
 
 def exact_match_rate(
@@ -34,9 +31,41 @@ def exact_match_rate(
             if pred_end_sample == real_end_sample:
                 matches += 1
             match_options.append(matches)
-            correct += max(match_options)
+            correct += max(match_options
+    match_rate = correct / total_indices
+    return correct, total_indices, match_rate
 
-    logger.warning(
-        f"Total correct predictions: {correct} out of {total_indices} ({correct / total_indices}%) match rate."
-    )
-    return correct, total_indices
+
+def f1_score(
+        real_start: Union[List[List[torch.Tensor]], torch.Tensor],
+        real_end: Union[List[List[torch.Tensor]], torch.Tensor],
+        pred_start: torch.Tensor,
+        pred_end: torch.Tensor
+):
+    all_f1 = []
+    for i, (pred_start_sample, pred_end_sample) in enumerate(zip(pred_start, pred_end)):
+        '''The list below list will store how many correct predictions (start+end) the algorithm makes for every
+        correct possible answer. E.g. if there are 3 possible correct answers, and the algorithm predicts start+end 
+        correctly for the first answer, only correct start of the second possible answer, and not the third 
+        possible answer the list will be [2, 1, 0]. We'll take take the max (in this case 2), as any correct possible
+        answer means our model made a correct prediction.'''
+
+        pred_indices = set(range(pred_start_sample, pred_end_sample + 1))
+
+        f1_options = []
+        # each sample might have j correct possible answers
+        for real_start_sample, real_end_sample in zip(real_start[i], real_end[i]):
+
+            real_indices = set(range(real_start_sample, real_end_sample + 1))  # consider adding int() around tensors
+            num_same_tokens = real_indices.intersection(pred_indices)
+
+            precision = num_same_tokens / len(pred_indices)
+            recall = num_same_tokens / len(real_indices)
+            f1_sample = (2 * precision * recall) / (precision + recall)
+            f1_options.append(f1_sample)
+        all_f1.append(max(f1_options))
+
+        average_f1 = np.mean(all_f1)
+        return all_f1, average_f1
+
+
