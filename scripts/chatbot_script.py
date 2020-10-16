abf7ad867684dadeb2f75a393c8e0459f3688f7f
AndreaSottana <48888970+AndreaSottana@users.noreply.github.com>
2020-10-16 12:38:17 +0100

    add chatboth script

---
+++ scripts/chatbot_script.py
@@ -0,0 +1,18 @@
+import torch
+from transformers import BertTokenizer
+from modules.bert_for_qa.user_interface import ChatBot
+
+
+if __name__ == '__main__':
+    tokenizer = BertTokenizer.from_pretrained("bert-base-cased", do_lower_case=False)
+    model = torch.load("../models/trained_model_full.pt", map_location=torch.device('cpu'))
+    context = """
+Boris Johnson is a British politician, author, and former 
+journalist who has served as Prime Minister of the United Kingdom and Leader of the Conservative Party since 2019. 
+He was Foreign Secretary from 2016 to 2018 and Mayor of London from 2008 to 2016. Johnson was Member of 
+Parliament (MP) for Henley from 2001 to 2008 and has been MP for Uxbridge and South Ruislip since 2015. 
+Ideologically, he identifies as a one-nation conservative.
+"""
+    bot = ChatBot(context, tokenizer, model)
+    answer = bot.answer(["Who is Boris Johnson?", "Since when has Johnson been MP for Uxbridge and South Ruislip?"])
+    print("Answer is: ", answer)
