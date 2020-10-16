1e4bb0ab257785349848852daaf03f89058ac42a
AndreaSottana <48888970+AndreaSottana@users.noreply.github.com>
2020-10-16 16:01:50 +0100

    fix bug in token counting

--- modules/bert_for_qa/user_interface.py
+++ modules/bert_for_qa/user_interface.py
@@ -53,8 +53,8 @@ class ChatBot:
             input_ids, token_type_ids, attention_masks, self.model, 1, disable_progress_bar=disable_progress_bar
         )
         predicted_answers = [
-            self.tokenizer.decode(input_ids[0, pred_start_i:pred_end_i + 1])
-            for pred_start_i, pred_end_i in zip(pred_start, pred_end)
+            self.tokenizer.decode(input_ids[i, pred_start_i:pred_end_i + 1])
+            for i, (pred_start_i, pred_end_i) in enumerate(zip(pred_start, pred_end))
         ]
         if len(predicted_answers) == 1:
             return predicted_answers[0]  # return answer as string instead of list if there is only one question
--- scripts/chatbot_script.py
+++ scripts/chatbot_script.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
     tokenizer = BertTokenizer.from_pretrained("bert-base-cased", do_lower_case=False)
     model = torch.load("../models/trained_model_full.pt", map_location=torch.device('cpu'))
     context = """
-Boris Johnson is a British politician, author, and former  journalist who has served as Prime Minister of the United 
-Kingdom and Leader of the Conservative Party since 2019. 
-He was Foreign Secretary from 2016 to 2018 and Mayor of London from 2008 to 2016. Johnson was Member of 
-Parliament (MP) for Henley from 2001 to 2008 and has been MP for Uxbridge and South Ruislip since 2015. 
-Ideologically, he identifies as a one-nation conservative.
-"""
+    Boris Johnson is a British politician, author, and former  journalist who has served as Prime Minister of the United 
+    Kingdom and Leader of the Conservative Party since 2019. 
+    He was Foreign Secretary from 2016 to 2018 and Mayor of London from 2008 to 2016. Johnson was Member of 
+    Parliament (MP) for Henley from 2001 to 2008 and has been MP for Uxbridge and South Ruislip since 2015. 
+    Ideologically, he identifies as a one-nation conservative.
+    """.replace("\n", " ")
+
     bot = ChatBot(context, tokenizer, model, max_len=300)
     questions = [
         "Who is Boris Johnson?",
